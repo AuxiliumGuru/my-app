9546b25631d9a0604527cd231f64853f4c4ccec4
AuxiliumGuru <khestermesa@gmail.com>
2024-06-26 21:16:15 +0800

    added a page.tsx to the projects folder

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import ProjectCard from '../../../components/ProjectCard';
+import Navbar from '../../../components/Navbar';
+
+export default function Project() {
+  return (
+
+    <div className="min-h-screen"> {/* Set background color for entire page */}   
+        <Navbar />
+
+        <div className="flex flex-row py-8 px-6">
+            <ProjectCard />
+        </div>
+        
+    </div>
+  )
+}
