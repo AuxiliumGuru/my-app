7bb22fa412cee37cbbaae6ee60ce0654e598af62
AuxiliumGuru <khestermesa@gmail.com>
2024-06-28 19:17:33 +0800

    modified the page.tsx and removed the projects component entirely from the file

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,20 +1,41 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import ProjectCard from '../../../components/ProjectCard';
 import Navbar from '../../../components/Navbar';
+import projectsData from '../../../constants/projects.json'; // Adjust the path as necessary
+import Link from 'next/link';
+import Image from 'next/image';
 
-export default function Project() {
+
+
+export default function ProjectPage() { // Renamed to avoid conflict with the Project interface
+  interface Project {
+    id: number;
+    name: string;
+    author: string;
+    description: string;
+    image: string;
+    link: string;
+  }
+  const projects: Project[] = projectsData.projects // Added type annotation
   return (
-
-    <div className="min-h-screen"> {/* Set background color for entire page */}   
-        <Navbar />
-
-        <div className="flex flex-row py-8 px-6">
-            <ProjectCard />
-            
-
-        </div>
-        
+    <div className="min-h-screen">
+      <Navbar />
+      <div className="flex flex-row py-8 px-6 flex-wrap"> {/* Added flex-wrap for responsiveness */}
+        {projects.map((project) => (
+          <Link key={project.id} href={project.link} passHref>
+            <div className="max-w-sm rounded-md overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300 cursor-pointer m-4">
+              <Image src={project.image} alt="Project Image" width={100} height={50} layout="responsive" />
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{project.name}</div>
+                <p className="text-gray-700 text-base">
+                  {project.author}
+                </p>
+                {/* <p className="text-gray-700 text-sm mb-2 px-6 pt-4 pb-2 opacity-0 hover:opacity-100 transition-opacity duration-300">
+                  {project.description}
+                </p> */}
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
