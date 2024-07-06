2e8cf2b45ecd2894d74809f27c974be11d0c89f0
AuxiliumGuru <khestermesa@gmail.com>
2024-07-06 22:54:31 +0800

    cented button

--- src/app/projects/apod/page.tsx
+++ src/app/projects/apod/page.tsx
@@ -3,6 +3,7 @@
 import Footer from '@/components/footer';
 import ProjectsNavbar from '@/components/ProjectsNavbar';
 import React, { useState } from 'react';
+import Image from 'next/image';
 
 interface APODData {
   date: string;
@@ -12,6 +13,7 @@ interface APODData {
   copyright?: string;
 }
 
+// Function to fetch APOD data
 export default function ProjectAPOD() {
   const [apodData, setApodData] = useState<APODData | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -42,22 +44,24 @@ export default function ProjectAPOD() {
       <ProjectsNavbar />
       <div className="flex-grow items-center justify-center min-h-screen p-10">
         {buttonVisible && ( // Step 4
-          <button 
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50" 
-            onClick={() => {
-              fetchAPODData();
-              setButtonVisible(false); // Step 3
-            }} 
-            disabled={isLoading}
-          >
-            {isLoading ? 'Loading...' : 'Generate Image'}
-          </button>
+           <div className="flex items-center justify-center pt-52"> {/* Add this wrapper */}
+            <button 
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50" 
+              onClick={() => {
+                fetchAPODData();
+                setButtonVisible(false);
+              }} 
+              disabled={isLoading}
+            >
+              {isLoading ? 'Loading...' : 'Generate Image'}
+            </button>
+         </div>
         )}
         {apodData && (
-          <div className="flex flex-col p-4 min-h-screen rounded-lg shadow-md mt-5">
+          <div className="flex flex-col items-center p-4 min-h-screen rounded-lg shadow-md mt-5">
             <h2 className="text-xl font-semibold">{apodData.title}</h2>
             <p className="text-gray-500">({apodData.date})</p>
-            <img className="mt-2 rounded" src={apodData.url} alt={apodData.title} />
+            <Image src={apodData.url} alt={apodData.title} width={500} height={300} />
             <p className="mt-2 text-gray-700">{apodData.explanation}</p>
             {apodData?.copyright && (
               <p className="mt-2 text-sm text-gray-500">Image Credit & Copyright: {apodData?.copyright}</p>
