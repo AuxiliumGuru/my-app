f5ece0dd4dc8daa785c4e5a8d809bed85fb5a535
AuxiliumGuru <khestermesa@gmail.com>
2024-07-06 15:09:20 +0800

    added a link to the image redirecting to the homepage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 export default function Navbar() {
   return (
     <header className="flex justify-between items-center py-4 px-8 shadow-md">
         <div className="text-gray-800 text-xl font-bold">
-            <Image
-                src="/favicon.ico"
-                alt="temp logo"
-                className="w-8 h-8 rounded-full object-cover lg:mr-5 md:w-8 md:h-8 lg:w-12 lg:h-12" // Increase image size on larger screens
-                width={50} // Add wid4
-                height={50} // Add height
-            />
+          <Link href="./"> {/* Wrap Image in Link to make it clickable */}
+              <Image
+                  src="/favicon.ico"
+                  alt="temp logo"
+                  className="w-8 h-8 rounded-full object-cover lg:mr-5 md:w-8 md:h-8 lg:w-12 lg:h-12" // Increase image size on larger screens
+                  width={50} // Add width
+                  height={50} // Add height
+              />
+          </Link>
         </div> {/* Adjust logo text as needed */}
 
         
